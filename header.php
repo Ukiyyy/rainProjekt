7b334e211ef7231b7a78a55b224a8dd3538b3e97
Ukiyyy <uros.mravljak@student.um.si>
2023-05-29 17:38:29 +0200

    last version

--- header.php
+++ header.php
@@ -33,7 +33,6 @@ $_SESSION['LAST_ACTIVITY'] = time();
                 <li><a class="nav-link" href="index.php?controller=paketnik&action=dodajPaketnik">Dodaj</a></li>
                 <li><a class="nav-link" href="index.php?controller=paketnik&action=zbrisiPaketnik">Izbrisi</a></li>
                 <li><a class="nav-link" href="index.php?controller=paketnik&action=spremeniIme">Spremeni ime</a>
-                <li><a class="nav-link" href="index.php?controller=logs&action=odkleni">Odkleni</a></li>
                 <li><a class="nav-link" href="index.php?controller=paketnik&action=posodiKljuc">Posodi kljuc</a></li>
             </ul>
         </ul>
